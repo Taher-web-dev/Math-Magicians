96e61f9f53d5b929a3849059fac93c3f6b6f2570
Taher <ttaherhagui@gmail.com>
2021-11-24 17:48:49 +0100

    Add events handlers.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,19 +2,39 @@ import React from 'react';
 import './style.css';
 import { ResultIcone } from './resultIcone';
 import { LigneOfButton } from './ligneOfButton';
+import calculate from '../logic/calculate';
 class CalculatorDesign extends React.Component {
   constructor(props){
     super(props)
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
   }
+  handleClick (e){
+    console.log('clicked...');
+    const target = e.target;
+    const child = target.children;
+    let targetElem;
+    if (child.length > 0){
+      targetElem = child[0];
+    }else {
+      targetElem = target;
+    }
+    const buttonName = targetElem.textContent;
+    this.setState(dataObject =>{
+      const newState = calculate(dataObject,buttonName);
+      return newState;
+    });
+  }
+  
   render(){
     return (
       <div className = "calculator-container">
-        <ResultIcone/>
-        <LigneOfButton textBtns = {['AC', '+/-', '%','÷']} order = {2} />
-        <LigneOfButton textBtns = {['7', '8', '9','x']} order = {3} />
-        <LigneOfButton textBtns = {['4', '5', '6','-']} order = {4} />
-        <LigneOfButton textBtns = {['1', '2', '3','+']} order = {5} />
-        <LigneOfButton textBtns = {['0', '.','=']} order = {6} />
+        <ResultIcone total = {this.state.total}/>
+        <LigneOfButton textBtns = {['AC', '+/-', '%','÷']} order = {2} handler = {this.handleClick}/>
+        <LigneOfButton textBtns = {['7', '8', '9','x']} order = {3} handler = {this.handleClick}/> 
+        <LigneOfButton textBtns = {['4', '5', '6','-']} order = {4} handler = {this.handleClick}/>
+        <LigneOfButton textBtns = {['1', '2', '3','+']} order = {5} handler = {this.handleClick}/>
+        <LigneOfButton textBtns = {['0', '.','=']} order = {6} handler = {this.handleClick}/>
       </div>
     );
   }
--- src/components/resultIcone.js
+++ src/components/resultIcone.js
@@ -2,8 +2,9 @@ import React from 'react';
 export class ResultIcone extends React.Component {
     constructor(props){
         super(props)
+        this.props = props;
     }
     render(){
-        return <div className = "calculator-result"> <span> 0</span> </div>
+        return <div className = "calculator-result"> <span> {this.props.total}</span> </div>
     }
 }
