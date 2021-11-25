d7ba78e69d7d29c17f274efa4dc0393826786563
Taher <ttaherhagui@gmail.com>
2021-11-25 19:07:30 +0100

    deconstruct object using javascript.

--- src/components/ligneOfButton.js
+++ src/components/ligneOfButton.js
@@ -1,23 +1,23 @@
 export const LigneOfButton = (props) => {
-  const btnsText = props.textBtns;
+  const {textBtns, order, handler} = props;
   let content;
-  const order = props.order;
-  if(btnsText.length === 4){
+  /*const order = props.order;*/
+  if(textBtns.length === 4){
     content = (
       <div className = 'command-line'  style = {{gridRow: `${order} / span 1`}}>
-        <div className ="simple-btn" onClick = {props.handler}><span>{btnsText[0]}</span></div>
-        <div className ="simple-btn" onClick = {props.handler}><span>{btnsText[1]}</span></div>
-        <div className ="simple-btn" onClick = {props.handler}><span>{btnsText[2]}</span></div>
-        <div className ="op-btn" onClick = {props.handler}><span>{btnsText[3]}</span></div>
+        <div className ="simple-btn" onClick = {handler}><span>{textBtns[0]}</span></div>
+        <div className ="simple-btn" onClick = {handler}><span>{textBtns[1]}</span></div>
+        <div className ="simple-btn" onClick = {handler}><span>{textBtns[2]}</span></div>
+        <div className ="op-btn" onClick = {handler}><span>{textBtns[3]}</span></div>
       </div>
     );
   }
-  if(btnsText.length === 3){
+  if(textBtns.length === 3){
     content = (
       <div className = 'command-line' style = {{gridRow: `${order} / span 1`}}>
-        <div className ="merge-btn" onClick = {props.handler}><span>{btnsText[0]}</span></div>
-        <div className ="simple-btn" onClick = {props.handler}><span>{btnsText[1]}</span></div>
-        <div className ="op-btn" onClick = {props.handler}><span>{btnsText[2]}</span></div>
+        <div className ="merge-btn" onClick = {handler}><span>{textBtns[0]}</span></div>
+        <div className ="simple-btn" onClick = {handler}><span>{textBtns[1]}</span></div>
+        <div className ="op-btn" onClick = {handler}><span>{textBtns[2]}</span></div>
       </div>
     );
   }
