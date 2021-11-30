e4255ed6057124cdfbdf57b9f0c7689065349c0c
Taher <ttaherhagui@gmail.com>
2021-11-30 11:02:33 +0100

    Prepare the SPA architecture.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import { CalculatorDesign } from "./components/Calculator";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App" >
-      <CalculatorDesign/>
-    </div>
+    <Router>
+      <div className="App" >
+        <ul>
+          <li>
+            <Link to = "/"> Home </Link>
+          </li>
+          <li>
+            <Link to = "/Calculator"> Calculator </Link>
+          </li>
+          <li>
+            <Link to = "/Quote"> Quote </Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path = "/">
+            <Homepage/>
+          </Route>
+          <Route path = "/Calculator">
+            <CalculatorDesign/>
+          </Route>
+          <Route path = "/Quote">
+            <Quotepage/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
