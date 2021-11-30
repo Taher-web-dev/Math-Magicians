216c458e7bf0d1e67c0aa4a45e09b3e80b1185f6
Taher <ttaherhagui@gmail.com>
2021-11-30 13:53:20 +0100

    add routers.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import { CalculatorDesign } from "./components/Calculator";
 import { Homepage } from "./components/Home";
+import { Quotepage } from "./components/Quote";
 import React from "react";
 import {
   BrowserRouter as Router,
+  Route,
   Switch,
-  Route
+  Link
 } from "react-router-dom";
 function App() {
   return (
     <Router>
       <div className="App" >
-        <ul>
+        <nav>
+         <ul>
           <li>
-            <Link to = "/"> Home </Link>
+            <Link to = "/">Home</Link>
           </li>
           <li>
-            <Link to = "/Calculator"> Calculator </Link>
+            <Link to = "/Calculator">Calculator</Link>
           </li>
           <li>
-            <Link to = "/Quote"> Quote </Link>
+            <Link to = "/Quote">Quote</Link>
           </li>
-        </ul>
+         </ul>
+        </nav>
         <Switch>
           <Route path = "/">
-            <Homepage/>
+            <Homepage />
           </Route>
-          <Route path = "/Calculator">
-            <CalculatorDesign/>
+          <Route path = "/Calculator" >
+            <CalculatorDesign />
           </Route>
           <Route path = "/Quote">
-            <Quotepage/>
+            <Quotepage />
           </Route>
         </Switch>
       </div>
---
+++ src/components/Quote.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export const Quotepage = ()=> {
+    return (
+     <p> 
+       Mathematics is not about numbers,
+       equations computations,or algorithms: it is about understanding. -William Paul Thurston
+     </p>
+    );
+}
