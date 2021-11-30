7641d7c5a67748a049ad28828fe1ba6ba0d3ea54
Taher <ttaherhagui@gmail.com>
2021-11-30 16:24:30 +0100

    Add styling for Quote component.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,17 @@
 import React from 'react';
 export const Quotepage = ()=> {
     return (
-     <p> 
+     <p style = {{
+      fontFamily: 'Montserrat,sans-serif',
+      textAlign:'center',
+      fontSize:'30px',
+      lineHeight: '1.6',
+      color: '#4ab',
+      fontWeight: 'bold',
+      marginTop: '15%',
+      width: '80%',
+      marginLeft: '10%'
+      }}> 
        Mathematics is not about numbers,
        equations computations,or algorithms: it is about understanding. -William Paul Thurston
      </p>
